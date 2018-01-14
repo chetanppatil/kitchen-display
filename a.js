61b6a68ded7ebd339d13fa3c518ed3b573efc7d6
Chetan <chetan0779@gmail.com>
2018-01-14 13:30:38 +0530

    report download csv added

--- a.js
+++ a.js
@@ -34,10 +34,49 @@ var result = _.map(a.productsData, (num) => {
   });
   num.quantity = _.isEmpty(qty) ? 0 : qty[0].sum;
 
-   let dnVal= _.filter(a.doneData, (val) => {
+  let dnVal = _.filter(a.doneData, (val) => {
     return val.code == num.code;
   });
   num.createdTillNow = _.isEmpty(dnVal) ? 0 : dnVal[0].sum;
   return num;
 })
-console.log(result);
+// console.log(result);
+
+
+
+
+var c = [{
+    "code": "jcw",
+    "name": "Jumbo Chicken Wrap",
+    "predictedValue": 2,
+    "quantity": 5,
+    "createdTillNow": 0
+  },
+  {
+    "code": "vl",
+    "name": "Vegetarian Lasagne",
+    "predictedValue": 5,
+    "quantity": 2,
+    "createdTillNow": 2
+  }
+];
+
+// var d = _.map(c, (num) => {
+//   let obj = {};
+//   obj["Dish Name"] = num.name;
+//   obj["Produced"] = num.createdTillNow;
+//   obj["Predicted"] = num.predictedValue;
+//   return obj;
+// })
+
+
+var d = [];
+for (var i = 0; i < c.length; i++) {
+  // console.log(c[i]);
+  var obj = {};
+  obj["Dish Name"] = c[i].name;
+  obj["Produced"] = c[i].createdTillNow;
+  obj["Predicted"] = c[i].predictedValue;
+  d.push(obj);
+}
+console.log(d);
--- public/app.js
+++ public/app.js
@@ -54,11 +54,87 @@ app.controller('myCtrl', function($scope, $http) {
       });
   };
 
+  /*======= JSON to CSV Code STARTS =========*/
+  $scope.downloadReport = function(showLabel) {
+    //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+    // var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+    var arrData = [];
+
+    for (var i = 0; i < $scope.orderData.length; i++) {
+      // console.log(c[i]);
+      var obj = {};
+      obj["Dish Name"] = $scope.orderData[i].name;
+      obj["Produced"] = $scope.orderData[i].createdTillNow;
+      obj["Predicted"] = $scope.orderData[i].predictedValue;
+      arrData.push(obj);
+    }
+
+    var CSV = '';
+    //Set Report title in first row or line
+    //CSV += ReportTitle + '\r\n\n';
+
+    //This condition will generate the Label/Header
+    if (showLabel) {
+      var row = "";
+
+      //This loop will extract the label from 1st index of on array
+      for (var index in arrData[0]) {
+        //Now convert each value to string and comma-seprated
+        row += index + ',';
+      }
+
+      row = row.slice(0, -1);
+      //append Label row with line break
+      CSV += row + '\r\n';
+    }
+
+    //1st loop is to extract each row
+    for (var i = 0; i < arrData.length; i++) {
+      var row = "";
+
+      //2nd loop will extract each column and convert it in string comma-seprated
+      for (var index in arrData[i]) {
+        row += '"' + arrData[i][index] + '",';
+        // row += '="' + arrData[i][index] + '",';
+        // row += "'" + arrData[i][index] + ",";
+        //"=""Data Here"""
+      }
+
+      row.slice(0, row.length - 1);
+
+      //add a line break after each row
+      CSV += row + '\r\n';
+    }
+
+    if (CSV == '') {
+      alert("Invalid data");
+      return;
+    }
+
+    //Generate a file name
+    var fileName = "Order_Report";
+
+    //Initialize file format you want csv or xls
+    var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+    //this trick will generate a temp <a /> tag
+    var link = document.createElement("a");
+    link.href = uri;
+
+    //set the visibility hidden so it will not effect on your web-layout
+    link.style = "visibility:hidden";
+    // link.download = fileName + ".csv";
+    link.download = fileName + ".csv";
+
+    //this part will append the anchor tag and remove it after automatic click
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+
+  }; //JSONToCSVConvertor
+
+  /*======= JSON to CSV Code ENDS =========*/
+
+
 
 });
-
-// $('form').submit(function(e) {
-//   e.preventDefault();
-//   var message = $('#chat_input').val();
-//   socket.emit('messages', message);
-// });
